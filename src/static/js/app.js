322f534c39f24276c7dc3a30154db2d62b29d8e2
brightseon <public.seon@gmail.com>
2022-07-13 22:54:50 +0900

    feat: Use bind mounts

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,9 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no todo items yet! Add one above!</p>
+                <p className="text-center">
+                    You have no todo items yet! Add one{' '}
+                </p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -106,7 +108,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
